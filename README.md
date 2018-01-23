0ca24fbb03a35a0ea5a3a44bc3877a35db790152
Bettina Junghahn <bettinajunghahn@Bettinas-MacBook-Pro.local>
2018-01-23 17:01:58 -0500

    adding read-me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Train-Scheduler
 Train Scheduler App
+
+The Train Schedule App allows you to view and add train schedules and times. This scheduler is for Florida Trains only.
+The App works by providing a form where the user can input a new train's data, and 2 tables that display the Northbound and Southbound trains' information.
+The train company can use this app to add and manage their trains in an organized fashion.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,36 +11,34 @@ messagingSenderId: "261979040305"
 
 firebase.initializeApp(config);
 
-
 const dbRef = firebase.database().ref('TrainSchedule/trains');
 
 // Button for adding trains
 $("#add-train-btn").click(function(event) { 
-  event.preventDefault();
+	event.preventDefault();
 
-  const newTrain = {
-    trainName: $("#train-name-input").val().trim(),
-    destination: $("#destination-input").val().trim(),
-    direction: $("#direction-input").val().trim(),
-    firstTrainTime: moment($("#first-train-input").val().trim(), "HH:mm").format("X"),
-    frequency: $("#frequency-input").val().trim()
-  };
+	const newTrain = {
+    	trainName: $("#train-name-input").val().trim(),
+    	destination: $("#destination-input").val().trim(),
+    	direction: $("#direction-input").val().trim(),
+    	firstTrainTime: moment($("#first-train-input").val().trim(), "HH:mm").format("X"),
+    	frequency: $("#frequency-input").val().trim()
+  	};
 
-  dbRef.push(newTrain);
-  // console.log(newTrain);
+	dbRef.push(newTrain);
+	// console.log(newTrain);
   
-  resetInputs();
-  
+	resetInputs();
 });
 
-// Firebase event for adding train to the database and a row in the html when a user adds an entry
+// Firebase event for adding train to the database and a row when a user adds an entry
 dbRef.on("child_added", function(childSnapshot, prevChildKey) {
 
-  // Train Info
-  const newTrain = childSnapshot.val();
-  // console.log(newTrain);
+	// Train Info
+	const newTrain = childSnapshot.val();
+	// console.log(newTrain);
   
-  // Calculate the next train
+	// Calculate the next train
 		const tFrequency = $("#frequency-input").val().trim();
 		const firstTime = moment($("#first-train-input").val().trim(), "HH:mm");
 		const firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
@@ -58,40 +56,43 @@ dbRef.on("child_added", function(childSnapshot, prevChildKey) {
 
 		// Next Train
 		const nextTrain = moment().add(tMinutesTillTrain, "minutes");
-		const nextTrainNewTime = moment.unix(nextTrain).format("HH:mm");
+		// const nextTrainNewTime = nextTrain.format("HH:mm");
 		// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 	newTrain.nextArrival = nextTrain;
   	console.log(newTrain.nextArrival);
 
-  // Calculate minutes away
-  newTrain.minAway = tMinutesTillTrain;
-  console.log(newTrain.minAway);
+	// Calculate minutes away
+	newTrain.minAway = tMinutesTillTrain;
+	console.log(newTrain.minAway);
 
-
-  // Determine which table to put train info in
-  if (newTrain.direction === "Northbound") {
-  	$("#nb-train-table > tbody").append(createTrainRow(newTrain));
-  }
-  else {
-  	$("#sb-train-table > tbody").append(createTrainRow(newTrain));
-    }
+	// Determine which table to put new train info in
+	if (newTrain.direction === "Northbound") {
+		$("#nb-train-table > tbody").append(createTrainRow(newTrain));
+	}
+	else {
+		$("#sb-train-table > tbody").append(createTrainRow(newTrain));
+	}
 
 });
 
-function createTrainRow(tra) {
-  const trow = $('<tr>');
-  trow.append(`<td>${tra.trainName}</td>`)
-      .append(`<td>${tra.destination}</td>`)
-      .append(`<td>${tra.frequency}</td>`)
-      .append(`<td>${tra.nextArrival}</td>`)
-      .append(`<td>${tra.minAway}</td>`)
 
-  return trow;
+// Function to add New Train row to table
+function createTrainRow(tra) {
+	const trow = $('<tr>');
+	trow.append(`<td>${tra.trainName}</td>`)
+    	.append(`<td>${tra.destination}</td>`)
+    	.append(`<td>${tra.frequency}</td>`)
+    	.append(`<td>${tra.nextArrival}</td>`)
+    	.append(`<td>${tra.minAway}</td>`)
+
+	return trow;
 }
 
+
+// Function to reset form inputs
 function resetInputs() {
-  $("form input:not([submit])").val('');
-  $("#train-name-input").focus();
+	$("form input:not([submit])").val('');
+	$("#train-name-input").focus();
 }
 
