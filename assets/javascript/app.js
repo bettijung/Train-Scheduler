d35f9f4ee8bd91e28241ee285ab33eb597112f56
Bettina Junghahn <bettinajunghahn@Bettinas-MacBook-Pro.local>
2018-01-23 11:53:01 -0500

    adding partially working code: git push origin master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,96 @@
 
 // Initialize Firebase
+var config = {
+apiKey: "AIzaSyAqiGphnLfCessqLlqaQHVzYTDm8CkZTps",
+authDomain: "train-scheduler-app-b9568.firebaseapp.com",
+databaseURL: "https://train-scheduler-app-b9568.firebaseio.com",
+projectId: "train-scheduler-app-b9568",
+storageBucket: "",
+messagingSenderId: "261979040305"
+};
+
+firebase.initializeApp(config);
+
+
+const dbRef = firebase.database().ref('TimeSheet/employees');
+
+// Button for adding trains
+$("#add-train-btn").click(function(event) { 
+  event.preventDefault();
+
+  const newTrain = {
+    trainName: $("#train-name-input").val().trim(),
+    destination: $("#destination-input").val().trim(),
+    direction: $("#direction-input").val().trim(),
+    firstTrainTime: moment($("#first-train-input").val().trim(), "HH:mm"),
+    frequency: $("#frequency-input").val().trim()
+  };
+
+  dbRef.push(newTrain);
+  // console.log(newTrain);
+  
+  resetInputs();
+  
+});
+
+// Firebase event for adding train to the database and a row in the html when a user adds an entry
+dbRef.on("child_added", function(childSnapshot, prevChildKey) {
+
+  // Train Info
+  const newTrain = childSnapshot.val();
+  // console.log(newTrain);
+  
+  // Calculate the next train
+		const tFrequency = $("#frequency-input").val().trim();
+		const firstTime = moment($("#first-train-input").val().trim(), "HH:mm");
+		const firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+		// console.log(firstTimeConverted);
+		const currentTime = moment();
+		// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+		const diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		// console.log("DIFFERENCE IN TIME: " + diffTime);
+		const tRemainder = diffTime % tFrequency;
+		// console.log(tRemainder);
+
+		// Minute Until Train
+		const tMinutesTillTrain = tFrequency - tRemainder;
+		// console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+		// Next Train
+		const nextTrain = moment().add(tMinutesTillTrain, "minutes");
+		// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+	newTrain.nextArrival = nextTrain;
+  	console.log(newTrain.nextArrival);
+
+  // Calculate minutes away
+  newTrain.minAway = tMinutesTillTrain;
+  console.log(newTrain.minAway);
+
+
+  // Determine which table to put train info in
+  if (newTrain.direction === "Northbound") {
+  	$("#nb-train-table > tbody").append(createTrainRow(newTrain));
+  }
+  else {
+  	$("#sb-train-table > tbody").append(createTrainRow(newTrain));
+    }
+
+});
+
+function createTrainRow(tra) {
+  const trow = $('<tr>');
+  trow.append(`<td>${tra.trainName}</td>`)
+      .append(`<td>${tra.destination}</td>`)
+      .append(`<td>${tra.frequency}</td>`)
+      .append(`<td>${tra.nextArrival}</td>`)
+      .append(`<td>${tra.minAway}</td>`)
+
+  return trow;
+}
+
+function resetInputs() {
+  $("form input:not([submit])").val('');
+  $("#train-name-input").focus();
+}
+
